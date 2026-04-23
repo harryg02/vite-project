cb04196f09d8e829166b516308a4f0c0714d374c
harryg02 <94257270+harryg02@users.noreply.github.com>
2026-04-23 13:06:53 -0400

    feat: add GitHub Actions workflow for static site deployment and configure base path in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/vite-project/', // ← change to your repo name, e.g. '/<REPO>/'
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
